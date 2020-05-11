63e69b3812f00d47cac8a03131cb28e21e7297d6
Anurodh Kumar <anu.ckp.13@gmail.com>
2020-05-11 13:09:11 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,36 +1,60 @@
-from tkinter import *
+from logging import exception
+
 from pytube import YouTube
 from tkinter.filedialog import *
-
-
-
-
+from tkinter import *
+from tkinter.messagebox import *
+from threading import *
 
 file_size = 0
 
-def startdownlod(url):
+
+def progress(stream=None, chunk=None, file_handel=None, remaining=None):
+    file_downloaded = (file_size - remaining)
+    per = (file_downloaded / file_size) * 100
+    dBtn.config(text="{}% downloaded".format(per))
+
+
+def startdownlod():
     global file_size
     try:
+        url = urlfield.get()
+        print(url)
+
+        dBtn.config(text='please wait...')
+        dBtn.config(state=DISABLED)
 
         path = askdirectory()
+        print(path)
 
-        ob = YouTube(url)
+        ob = YouTube(url, on_progress_callback=progress)
 
         # strms = ob.streams.all()
 
         # for s in strms:
         # print(s)
         strms = ob.streams.first()
+        file_size = strms.filesize
+        print(file_size)
         # print(strms.filesize)
         # print(strms.title)
         # print(ob.title)
         # print(ob.description
         strms.download(path)
         print("Done")
-
+        dBtn.config(text="done")
+        dBtn.config(state=NORMAL)
+        showinfo("download finished", "downloded")
+        urlfield.delete(0, END)
     except exception as e:
         print(e)
         print("error")
+
+
+def startdownloadthread():
+    thread = Thread(target=startdownlod)
+    thread.start()
+
 
 main = Tk()
 
@@ -40,16 +64,15 @@ main.iconbitmap('download.ico')
 
 main.geometry('500x600')
 
-file=PhotoImage(file ='download2.png')
+file = PhotoImage(file='download2.png')
 
+headingIcon = Label(main, image=file)
+headingIcon.pack(side=TOP)
 
+urlfield = Entry(main, font=('verdana', 18), justify=CENTER)
+urlfield.pack(side=TOP, fill=X, padx=10)
 
-
-
-headingIcon=Lable(main,image='file')
-headingIcon.pack(side=top)
-
-urlfield = Entry(main)
-urlfield.pack(side=top)
+dBtn = Button(main, text="start download", font=("verdana", 18), command=startdownloadthread)
+dBtn.pack(side=TOP, pady=10)
 
 main.mainloop()
